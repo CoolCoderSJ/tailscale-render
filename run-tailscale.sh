0915508183df47110b74a1ea4110de59d1cd315a
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2024-08-07 23:49:24 -0400

    Update run-tailscale.sh

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
+/render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
 PID=$!
 
-until /render/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname="${RENDER_SERVICE_NAME}" --advertise-exit-node; do
+until /render/tailscale up --hostname="${RENDER_SERVICE_NAME}" --advertise-exit-node; do
   sleep 0.1
 done
 export ALL_PROXY=socks5://localhost:1055/
