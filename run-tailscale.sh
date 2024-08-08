082c4cf4bf9d6de1df0929ad22ae3b7b2bf44e6d
Shuchir Jain <53063247+CoolCoderSJ@users.noreply.github.com>
2024-08-08 00:30:32 -0400

    Update run-tailscale.sh

--- run-tailscale.sh
+++ run-tailscale.sh
@@ -3,11 +3,9 @@
 /render/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 --outbound-http-proxy-listen=localhost:1055 &
 PID=$!
 
+/render/tailscale up --hostname="${RENDER_SERVICE_NAME}" --advertise-exit-node &
 /render/tailscale web --listen 80 &
 
-until /render/tailscale up --hostname="${RENDER_SERVICE_NAME}" --advertise-exit-node; do
-  sleep 0.1
-done
 export ALL_PROXY=socks5://localhost:1055/
 tailscale_ip=$(/render/tailscale ip)
 echo "Tailscale is up at IP ${tailscale_ip}"
